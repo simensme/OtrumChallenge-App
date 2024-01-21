019ac5a58fd74fc7328de7541abbaa870c9676de
Simen Smehaugen <simdevgen@outlook.com>
2024-01-21 17:06:15 +0100

    amended styling and added buttons

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,9 @@ import NameList from './components/NameList.vue';
 </script>
 
 <template>
-  <h1>helloaefawef3f</h1>
   <NameList/>
 </template>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
-html {
-  background: rgb(227, 227, 227);
-  box-sizing: border-box;
-}
-
-body {
-  margin: 0;
-  min-height: 100vh;
-  font-family: 'Roboto', sans-serif;
-  display: flex;
-}
 
 </style>
--- src/components/NameList.vue
+++ src/components/NameList.vue
@@ -3,19 +3,12 @@
 </script>
 
 <template>
-    <div class="header">
-        <h1>Navneliste</h1>
-    </div>
-    <div>
+    <div class="main">
+        <div class="header">
+            <h1>NameList</h1>
+        </div>
         <table>
-            <thead class="item-list">
-                <tr>
-                    <th class="navn-column">Navn</th>
-                    <th class="alder-column">Alder</th>
-                    <th class="x-column">Slett</th>
-                </tr>
-            </thead>
-            <tbody class="item-list">
+            <tbody>
                 <tr>
                     <td>George Bush</td>
                     <td>25</td>
@@ -31,91 +24,145 @@
                     <td>50</td>
                     <td>X</td>
                 </tr>
+                <tr>
+                    <td>Winston Churchill</td>
+                    <td>95</td>
+                    <td>X</td>
+                </tr>
+                <tr>
+                    <td>James Cage</td>
+                    <td>39</td>
+                    <td>X</td>
+                </tr>
             </tbody>
         </table>
-    </div>
-    <div class="add-container">
-        <button id="save">Add</button>
-    </div>
-    <div class="reload-save-container">
-        <button id="reload">Reload</button>
-        <button id="save">Save</button>
+        <div class="button-container">
+            <div class="add-container">
+                <button id="add">+ Add</button>
+            </div>
+            <div class="reload-save-container">
+                <button id="reload">Reload</button>
+                <button id="save">Save</button>
+            </div>
+        </div>
     </div>
 </template>
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
+html {
+    background: rgb(227, 227, 227);
+    box-sizing: border-box;
+}
+
 body {
+    min-height: 100vh;
+    font-family: 'Roboto', sans-serif;
     display: flex;
-    align-items: center;
     justify-content: center;
-    font-family: Arial, sans-serif;
+    align-items: center;
     margin: 0;
     padding: 0;
 }
-h1 {
-    margin: 0;
+
+.main {
+    width: 38%;
+    margin: 0 auto;
 }
 
+/* Title */
 .header {
-    background-color: #a9a9a9;
+    background-color: #909090;
     color: #fff;
     text-align: center;
-    border-radius: 10px;
-    padding: 10px;
-
+    border-radius: 20px;
+    width: 96%;
+    margin: 0 auto;
 }
 
+h1 {
+    padding: 10px;
+}
+
+/* Table Content */
 table {
+    border-spacing: 10px;
+    width: 100%;
+    table-layout: fixed;
+}
+
+tbody {
     width: 100%;
     border-collapse: collapse;
     margin-top: 20px;
-}
-
-th,
-td {
-    border: 1px solid #ddd;
-    padding: 8px;
-    text-align: left;
-}
-
-th {
-    background-color: #333;
-    color: #fff;
-}
-
-.navn-column {
-    width: 60%;
-    /* Adjust the width for the "Navn" column */
-}
-
-.alder-column,
-.x-column {
-    width: 20%;
-    /* Adjust the width for the "Alder" and "X" columns */
-}
-
-.item-list {
     background-color: #f9f9f9;
+}
+
+td {
+    border: 3px solid #299026;
+    padding: 14px;
+    white-space: nowrap;
+}
+
+td:first-child {
+    width: 90%;
+}
+
+td:nth-child(2) {
+    width: 6%;
+}
+
+td:nth-child(3) {
+    width: 4%;
+}
+
+
+/* Buttons */
+.button-container {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 10px;
 }
 
 .add-container,
 .reload-save-container {
-    margin-top: 20px;
     text-align: center;
+}
+
+.add-container {
+    margin-left: 5px;
+}
+
+.reload-save-container {
+    display: flex;
+    gap: 10px;
+    justify-content: flex-end;
+    margin-top: 50px;
+    margin-right: 5px;
 }
 
 button {
     padding: 10px;
     margin: 5px;
     cursor: pointer;
+    font-family: 'Roboto', sans-serif;
+    font-size: 18px;
+    color: #000000;
+    background: #ffffff;
+    padding: 10px 20px;
+    border: solid #2fab1f 3px;
+    text-decoration: none;
+    position: relative;
 }
 
-#reload {
-    background-color: #4285f4;
-    color: #fff;
+button:hover {
+    background: rgb(199, 199, 199);
 }
 
-#save {
-    background-color: #34a853;
-    color: #fff;
+button:active {
+    bottom: -2px;
+    right: -2px;
+
 }
+
 </style>
